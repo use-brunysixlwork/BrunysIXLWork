93f5868b11f847e0e90b958fb7825ddbfc7a6b32
Brunys <brunys@brunysixl.xyz>
2025-01-20 16:20:54 -0500

    am and pm

--- public/assets/js/time.js
+++ public/assets/js/time.js
@@ -1,11 +1,17 @@
 function updateTime() {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
+    let hours = now.getHours();
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
-    const currentTime = `${hours}:${minutes}:${seconds}`;
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    
+    
+    hours = hours % 12;
+    hours = hours ? String(hours).padStart(2, '0') : '12'; 
+    
+    const currentTime = `${hours}:${minutes}:${seconds} ${ampm}`;
     document.getElementById("timeDisplay").textContent = currentTime;
 }
 
 setInterval(updateTime, 1000);
-updateTime(); 
+updateTime();

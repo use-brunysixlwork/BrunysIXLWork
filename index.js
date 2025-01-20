aef6f0313a9e3f252e94453a7f7f6b182f6c5fa1
Brunys <brunys@brunysixl.xyz>
2025-01-19 22:22:53 -0500

    node

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const { createBareServer } = require("@tomphttp/bare-server-node");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const routes = [
+  { path: "/", file: "index.html" },
+  { path: "/g", file: "gms.html" },
+  { path: "/a", file: "aps.html" },
+  { path: "/s", file: "settings.html" },
+  { path: "/p", file: "proxy.html" },
+  { path: "/404", file: "404.html" },
+];
+
+routes.forEach((route) => {
+  app.get(route.path, (req, res) => {
+    res.sendFile(path.join(__dirname, "public", route.file));
+  });
+});
+
+app.use((req, res) => {
+  res.redirect("/404");
+});
+
+const bareServer = createBareServer("/bare/");
+const server = http.createServer((req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});

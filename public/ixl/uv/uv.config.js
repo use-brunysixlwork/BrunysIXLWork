4c0de6b7c51c9750b08319da0323e1a35f4fa30d
Brunys <brunys@brunysixl.xyz>
2025-02-01 23:31:30 -0500

    bare server change

--- public/ixl/uv/uv.config.js
+++ public/ixl/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/ixl/bw/',
-    bare:'/bare/',
+    bare:'/b/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/ixl/uv/uv.handler.js',

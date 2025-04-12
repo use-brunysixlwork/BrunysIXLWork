dcb5209844ee8fae1c9b91fa0147d34c871626d4
BrunysIXLWork <brunyssite@proton.me>
2025-04-12 19:10:59 -0400

    j

--- public/assets/js/settings/themesplus.js
+++ public/assets/js/settings/themesplus.js
@@ -1,13 +1,22 @@
-document.getElementById('theme-selector').addEventListener('change', function () {
-    const selectedTheme = this.value;
 
-    document.body.className = selectedTheme;
-
+window.addEventListener('DOMContentLoaded', function () {
+    const savedTheme = localStorage.getItem('selectedTheme');
     const nameElement = document.getElementById('site-name');
 
-    if (selectedTheme === 'theme-jimmy') {
-        nameElement.textContent = "Jimmys Games";
-    } else {
-        nameElement.textContent = "BrunysIXLWork";
+    if (savedTheme) {
+        document.body.className = savedTheme;
+        document.getElementById('theme-selector').value = savedTheme;
+
+        nameElement.textContent = (savedTheme === 'theme-jimmy') ? "Jimmys Games" : "BrunysIXLWork";
     }
+});
+
+
+document.getElementById('theme-selector').addEventListener('change', function () {
+    const selectedTheme = this.value;
+    document.body.className = selectedTheme;
+    localStorage.setItem('selectedTheme', selectedTheme);
+
+    const nameElement = document.getElementById('site-name');
+    nameElement.textContent = (selectedTheme === 'theme-jimmy') ? "Jimmys Games" : "BrunysIXLWork";
 });

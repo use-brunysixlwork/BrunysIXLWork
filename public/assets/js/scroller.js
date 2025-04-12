5a44cad34daa10a1028c8e6076da3ec0cb7c4475
BrunysIXLWork <brunyssite@proton.me>
2025-04-12 16:16:18 -0400

    brunysixlwork hot fix v0.1

--- public/assets/js/scroller.js
+++ public/assets/js/scroller.js
@@ -1,8 +1,12 @@
-  const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.search);
   const scrollTarget = urlParams.get('scroll'); 
 
   if (scrollTarget && scrollTarget.startsWith('#')) {
     window.addEventListener('load', () => {
+     
+      window.scrollTo(0, 0);
+
+    
       const element = document.querySelector(scrollTarget);
       if (element) {
         element.scrollIntoView({ behavior: 'smooth' });

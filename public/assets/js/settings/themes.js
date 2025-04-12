cfb5b11c2a09c166f124ac484bb7265f44dda37c
BrunysIXLWork <brunyssite@proton.me>
2025-04-12 15:47:36 -0400

    themes beta

---
+++ public/assets/js/settings/themes.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const selector = document.getElementById("theme-selector");
+    const savedTheme = localStorage.getItem("selected-theme");
+
+
+    if (savedTheme && savedTheme !== "default") {
+        document.body.classList.add(savedTheme);
+        selector.value = savedTheme;
+    }
+
+    selector.addEventListener("change", () => {
+    
+        document.body.classList.remove("theme-light", "theme-blue");
+
+        const selected = selector.value;
+
+        if (selected === "default") {
+            localStorage.removeItem("selected-theme");
+        } else {
+            document.body.classList.add(selected);
+            localStorage.setItem("selected-theme", selected);
+        }
+    });
+});

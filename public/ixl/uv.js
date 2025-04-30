7393b5d937ac8ec7f84dba04088f732e3c3c0165
BrunysIXLWork <brunyssite@proton.me>
2025-04-29 20:37:49 -0400

    Delete public/ixl directory

--- public/ixl/uv.js
+++
@@ -1,8 +0,0 @@
-importScripts('/ixl/uv/uv.bundle.js');
-importScripts('/ixl/uv/uv.config.js');
-importScripts('/ixl/uv/uv.sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
-
-const sw = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
